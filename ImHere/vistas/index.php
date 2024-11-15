91b9b6be371374fcb37741ee36f0f3465db78c1c
Diego Jesus <s21120187@alumnos.itsur.edu.mx>
2024-11-14 21:46:05 -0600

    Fin segundo Sprint

--- ImHere/vistas/index.php
+++ ImHere/vistas/index.php
@@ -58,8 +58,7 @@
         <h1 class="display-4" id="Conocenos">¿Quiénes somos?</h1>
         <p class="lead" style="margin-left: 25%; margin-right: 25%;">
             Somos un equipo de ingenieros apasionados que, inspirados por la innovación, hemos desarrollado
-            una aplicación web revolucionaria. Esta plataforma moderna y eficiente utiliza la tecnología NFC para
-            simplificar el proceso de toma de asistencia. Estamos comprometidos en brindar una solución intuitiva y
+            una aplicación web revolucionaria. Estamos comprometidos en brindar una solución intuitiva y
             avanzada que transforme por completo la forma en que se realiza el pase de lista. Nuestro objetivo es
             eliminar la complejidad y las molestias asociadas con este procedimiento, permitiendo a profesores,
             organizadores de eventos y administradores gestionar la asistencia de manera rápida, precisa y sin
@@ -78,7 +77,7 @@
                         <img class="card-img-top" src="imgs/fondo1.jpg" alt="Card image cap">
                         <div class="card-body">
                             <h3>Rápido y sencillo</h3>
-                            <p class="card-text">Acerca tu tarjeta al sensor, nosotros hacemos el resto.</p>
+                            <p class="card-text"></p>
                         </div>
                     </div>
                 </div>
@@ -87,7 +86,7 @@
                         <img class="card-img-top" src="imgs/fondo2.jpg" alt="Card image cap">
                         <div class="card-body">
                             <h3>Preciso y confiable</h3>
-                            <p class="card-text">Elimina el riesgo de errores humanos o falsificaciones.</p>
+                            <p class="card-text"></p>
                         </div>
                     </div>
                 </div>
@@ -96,7 +95,7 @@
                         <img class="card-img-top" src="imgs/fondo3.jpg" alt="Card image cap">
                         <div class="card-body">
                             <h3>Flexible y adaptable</h3>
-                            <p class="card-text">Se puede usar en oficinas, aulas, eventos y más.</p>
+                            <p class="card-text"></p>
                         </div>
                     </div>
                 </div>
@@ -117,10 +116,7 @@
                                 <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mes</small>
                                 </h1>
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>No cuenta con tarjetas NFC</li>
-                                    <li>No cuenta con sensores NFC</li>
-                                    <li>Soporte 24/7</li>
-                                    <li>Acceso al centro de ayuda</li>
+                                    
                                 </ul>
                                 <button type="button" class="btn btn-lg btn-block btn-outline-primary">Inicia
                                     gratis</button>
@@ -134,10 +130,7 @@
                                 <h1 class="card-title pricing-card-title">$500 <small class="text-muted">/
                                         mes</small></h1>
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>Usuarios ilimitados</li>
-                                    <li>Hasta 20 sensores NFC</li>
-                                    <li>Hasta 2000 tarjetas NFC</li>
-                                    <li>Soporte 24/7</li>
+                                    
                                 </ul>
                                 <button type="button" class="btn btn-lg btn-block btn-primary">Contáctanos</button>
                             </div>
@@ -150,10 +143,7 @@
                                 <h1 class="card-title pricing-card-title">$1000 <small class="text-muted">/
                                         mes</small></h1>
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>Usuarios ilimitados</li>
-                                    <li>Hasta 50 sensores NFC</li>
-                                    <li>Hasta 20000 tarjetas NFC</li>
-                                    <li>Soporte 24/7</li>
+                                    
                                 </ul>
                                 <button type="button" class="btn btn-lg btn-block btn-primary">Contáctanos</button>
                             </div>
